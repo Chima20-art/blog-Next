1adabad207e05c8a9422cc105e608ddf3db9dfd8
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2021-11-09 16:35:39 +0200

    addede slug.module.css .

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -10,7 +10,7 @@ import Header from "../../components/header";
 import Footer from "../../components/footer/footer";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-import styles from "../../styles/Home.module.css";
+import styles from "../../styles/slug.module.css";
 const Post = (props) => {
   const slug = props.slug;
   const post = props.post;
